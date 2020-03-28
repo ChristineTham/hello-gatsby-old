4297689af487efa41b654de649430006e4c5045c
Christine Tham <chris.tham@hellotham.com>
2020-03-28 22:05:51 +1100

    Moved stories

--- src/stories/HelloBar.stories.tsx
+++ src/stories/HelloBar.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { muiTheme } from 'storybook-addon-material-ui'
 
-import HelloBar from './HelloBar'
+import HelloBar from '../components/HelloBar'
 
 // eslint-disable-next-line no-underscore-dangle
 // global.__PATH_PREFIX__ = ''
